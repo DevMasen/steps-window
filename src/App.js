0da6462bdc6e12b1424806d8387263244a60207f
DevMasen <mohammadwh400@gmail.com>
2025-07-29 12:11:29 +0330

    close button added with react fragment

--- src/App.js
+++ src/App.js
@@ -9,25 +9,32 @@ const messages = [
 function App() {
 	const step = 1;
 	return (
-		<div className="steps">
-			<div className="numbers">
-				<div className={step >= 1 ? 'active' : ''}>1</div>
-				<div className={step >= 2 ? 'active' : ''}>2</div>
-				<div className={step >= 3 ? 'active' : ''}>3</div>
-			</div>
-			<p className="message">
-				Step {step}: {messages[step - 1]}
-			</p>
+		<>
+			<div className="close">&#10006;</div>
+			<div className="steps">
+				<div className="numbers">
+					<div className={step >= 1 ? 'active' : ''}>1</div>
+					<div className={step >= 2 ? 'active' : ''}>2</div>
+					<div className={step >= 3 ? 'active' : ''}>3</div>
+				</div>
+				<p className="message">
+					Step {step}: {messages[step - 1]}
+				</p>
 
-			<div className="buttons">
-				<button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
-					<span>Previous</span>
-				</button>
-				<button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
-					<span>Next</span>
-				</button>
+				<div className="buttons">
+					<button
+						style={{ backgroundColor: '#7950f2', color: '#fff' }}
+					>
+						<span>Previous</span>
+					</button>
+					<button
+						style={{ backgroundColor: '#7950f2', color: '#fff' }}
+					>
+						<span>Next</span>
+					</button>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
